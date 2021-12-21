40c65aecd34a78fb4e3e0c932bcea238632e9da6
TuanVu1607 <tuanvutran369@gmail.com>
2021-12-21 20:30:20 +0700

    Up code source

---
+++ source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
+int tong(int a, int b){
+	return a+b;
+}
+int hieu(int a, int b);
+int tich(int a,int b){
+	return a*b;
+}
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap so nguyen 1: ";
+	cin >> a;
+	cout << "Nhap so nguyen 2: ";
+	cin >> b;
+	cout << "Hieu 2 so la: ";
+	cin >> hieu(a,b);
+	cout << "Tong 2 so la: ";
+	cin >> tong(a,b);
+	cout << "Tich 2 so la: ";
+	cin >> tich(a,b);
+	system("pause");
+	return 0;
+}
